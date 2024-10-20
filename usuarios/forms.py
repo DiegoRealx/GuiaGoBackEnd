e3489ec377a1e9950be4b72f701f55afefc0b6b1
Priscila Xavier <priscilaxavier672@gmail.com>
2024-10-20 18:04:27 -0300

    Adiciona o cadastro de forma funcional

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -3,7 +3,8 @@ from .models import Usuario
 
 class FormularioCadastroUsuario(forms.ModelForm):
     senha = forms.CharField(widget=forms.PasswordInput)
-
+    confirma_senha = forms.CharField(label="Confirme sua senha", widget=forms.PasswordInput)
+    
     class Meta:
         model = Usuario
         fields = ['email', 'nome_completo', 'telefone', 'senha', 'genero']
