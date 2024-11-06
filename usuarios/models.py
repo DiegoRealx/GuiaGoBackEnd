85fdfebd8484e30079b75648c106a85e673ea379
Diego Real <110725353+DiegoRealx@users.noreply.github.com>
2024-11-06 11:30:55 -0300

    alteração nas models e no html de preferencias e no css tbm

--- usuarios/models.py
+++ usuarios/models.py
@@ -16,16 +16,14 @@ class Usuario(models.Model):
         ]
     )
 
-    
     INTERESSES_CHOICES = [
         ('Praia', 'Praia'),
-        ('Cachoeira', 'Cachoeira'),
         ('Natureza', 'Natureza'),
         ('Cultura Local', 'Cultura Local'),
-        ('Trilha', 'Trilha'),
-        ('Passeios em Família', 'Passeios em Família'),
+        ('Aventura', 'Aventura'),
+        ('Compras', 'Compras'),
     ]
-    
+
     GASTRONOMIA_CHOICES = [
         ('Culinária Oriental', 'Culinária Oriental'),
         ('Vegetariano', 'Vegetariano'),
@@ -33,13 +31,13 @@ class Usuario(models.Model):
         ('Vegano', 'Vegano'),
         ('Frutos do Mar', 'Frutos do Mar'),
     ]
-    
+
     ESTILO_CHOICES = [
-        ('Aventura', 'Aventura'),
         ('Relaxante', 'Relaxante'),
         ('Cultural', 'Cultural'),
         ('Família', 'Família'),
         ('Luxuoso', 'Luxuoso'),
+        ('Gastronômico', 'Gastronômico'),
     ]
 
     interesses = models.CharField(max_length=50, choices=INTERESSES_CHOICES, blank=True)
@@ -57,25 +55,26 @@ class Usuario(models.Model):
     def __str__(self):
         return self.email
 
+
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
     localizacao = models.CharField(max_length=255)
     cidade = models.CharField(max_length=100)
     imagem = models.ImageField(upload_to='imagens_pontos_turisticos/', blank=True, null=True)
-    
+
     interesse_praia = models.BooleanField(default=False)
     interesse_natureza = models.BooleanField(default=False)
     interesse_cultura_local = models.BooleanField(default=False)
     interesse_aventura = models.BooleanField(default=False)
     interesse_compras = models.BooleanField(default=False)
-    
+
     culinaria_oriental = models.BooleanField(default=False)
     vegetariano = models.BooleanField(default=False)
     churrasco = models.BooleanField(default=False)
     vegano = models.BooleanField(default=False)
     frutos_do_mar = models.BooleanField(default=False)
-    
+
     estilo_relaxante = models.BooleanField(default=False)
     estilo_cultural = models.BooleanField(default=False)
     estilo_familia = models.BooleanField(default=False)
